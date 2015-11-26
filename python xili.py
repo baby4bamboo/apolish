af3df232dacf0694504596d4a1e02079dcdd9b1c
bayao <bayao@cisco.com>
2015-11-26 10:42:02 +0800

    rm test.py

--- python xili.py
+++
@@ -1,9 +0,0 @@
-import re,urllib2
-url="http://www.pythonchallenge.com/pc/def/equality.html"
-thestring=urllib2.urlopen(url).read()
-thepatten='[a-z][A-Z]{3}([a-z])[A-Z]{3}[a-z]'
-m=re.findall(thepatten,thestring)
-print ''.join(m)
-
-
-
--- test.py
+++
@@ -1,3 +0,0 @@
-import  re
-m = re.search("output_(\d{3})", "output_1986.txt")
-print(m)
